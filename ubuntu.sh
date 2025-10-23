8781c29ddb78434cc21e2e0f1b742a9f878f5028
xloem <0xloem@gmail.com>
2025-10-23 23:06:45 +0000

    -

---
+++ ubuntu.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt update
+sudo apt install -y python3.10 python3.10-venv python3.10-dev
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
+sudo update-alternatives --set python3 /usr/bin/python3.10
