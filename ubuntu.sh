1df9455cf69e29bfe227fa195a1e8adb4dd3252f
xloem <0xloem@gmail.com>
2025-10-23 23:13:04 +0000

    -

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,3 +5,4 @@ sudo apt update
 sudo apt install -y python3.10 python3.10-venv python3.10-dev
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
 sudo update-alternatives --set python3 /usr/bin/python3.10
+sudo apt install -y google-perftools libtcmalloc-minimal4t64
